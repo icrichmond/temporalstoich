7ee39b45c65e3596b90b7791615c2c499667992d
icrichmond <icrichmond@mun.ca>
2020-10-14 15:41:58 -0230

    GDD for each sampling date using 2 - GDD.R

--- code/2 - GDD.R
+++ code/2 - GDD.R
@@ -9,7 +9,7 @@
 
 # load packages 
 library(easypackages)
-easypackages::libraries("tidyverse", "pollen")
+easypackages::libraries("tidyverse", "pollen", "lubridate")
 
 #### Data Preparation ####
 # load datasets 
@@ -19,6 +19,7 @@ tn2016 <- read_csv("input/GDD/TNNP_2016_R.csv")
 tn2017 <- read_csv("input/GDD/TNNP_2017_R.csv")
 
 # drop NAs in TN 2017 temperature data (all in January - OK that they are missing)
+l2017 <- drop_na(l2017, MeanTemp)
 tn2017 <- drop_na(tn2017, MeanTemp)
 
 #### Growing Degree Days ####
@@ -51,6 +52,11 @@ l2016 <- subset(l2016, select = -c(X1))
 l2017 <- add_column(l2017, Station = "Lethbridge")
 tn2016 <- add_column(tn2016, Station = "TNNP")
 tn2017 <- add_column(tn2017, Station = "TNNP")
+# convert dates to actual dates 
+l2016$Date_Time <- as_date(l2016$Date_Time)
+l2017$Date_Time <- as_date(l2017$Date_Time)
+tn2016$Date_Time <- as_date(tn2016$Date_Time)
+tn2017$Date_Time <- as_date(tn2017$Date_Time)
 # merge datasets 
 all <- rbind(l2016, l2017, tn2016, tn2017)
 # save csv 
@@ -60,3 +66,55 @@ write.csv(all, "input/Weather_2016_2017_R.csv")
 #### Sampling Days ####
 # now we are going to extract the GDD value for each day of sampling for every measurement of elemental 
 # composition we have across the four sites and study species
+# load in sampling dates for all sites in 2016 and 2017 
+s2016 <- read_csv("input/GDD/Stoich_Height_2016.csv")
+s2017 <- read_csv("input/GDD/Stoich_Height_2017.csv")
+# we only care about the four study species, subset to remove others 
+s2016 <- filter(s2016, Species=="ABBA" | Species == "ACRU" | Species == "BEPA" | Species == "VAAN")
+s2017 <- filter(s2017, Species=="ABBA" | Species == "ACRU" | Species == "BEPA" | Species == "VAAN")
+
+
+# we want to extract the GDD on the day of sampling for each sample. Lethbridge weather station is used 
+# for Bloomfield study site and TNNP weather station is used for Unicorn, TNNP, and Dunphys Pond study sites 
+# simplify dataframes for easier extraction 
+s2016 <- subset(s2016, select = c(TimeStamp, SiteName, TrapLocation, PlotName, Species))
+s2017 <- subset(s2017, select = c(TimeStamp, SiteName, TrapLocation, PlotName, Species))
+
+# convert timestamps to actual dates and times in R 
+s2016$TimeStamp <- as_datetime(s2016$TimeStamp)
+s2017$TimeStamp <- as_datetime(s2017$TimeStamp)
+
+# repeated observations in the dataset - remove 
+s2016 <- s2016 %>% 
+  group_by(SiteName, TrapLocation, Species) %>% 
+  filter(row_number()==1)
+
+s2017 <- s2017 %>% 
+  group_by(SiteName, TrapLocation, Species) %>% 
+  filter(row_number()==1)
+
+# extract the growing degree days for each of these observations 
+# subset Bloomfield because it has a different weather station than the rest 
+bl2016 <- filter(s2016, SiteName=="Bloomfield")
+bl2016 <- add_column(bl2016, Date_Time = date(bl2016$TimeStamp))
+bl2016 <- inner_join(bl2016, l2016, by="Date_Time")
+
+bl2017 <- filter(s2017, SiteName=="Bloomfield")
+bl2017 <- add_column(bl2017, Date_Time = date(bl2017$TimeStamp))
+bl2017 <- inner_join(bl2017, l2017, by="Date_Time")
+
+tnundp2016 <- filter(s2016, SiteName=="Dumphys Pond" | SiteName=="TNNP North" | SiteName=="Unicorn")
+tnundp2016 <- add_column(tnundp2016, Date_Time = date(tnundp2016$TimeStamp))
+tnundp2016 <- inner_join(tnundp2016, tn2016, by="Date_Time")
+
+tnundp2017 <- filter(s2017, SiteName=="Dumphys Pond" | SiteName=="TNNP North" | SiteName=="Unicorn")
+tnundp2017 <- add_column(tnundp2017, Date_Time = date(tnundp2017$TimeStamp))
+tnundp2017 <- inner_join(tnundp2017, tn2017, by="Date_Time")
+
+
+# merge all four datasets together 
+extracted <- rbind(bl2016, bl2017, tnundp2016, tnundp2017)
+# keep important columns
+extracted <- subset(extracted, select = c(PlotName, Species, Year, Date_Time, GDD))
+# save csv
+write.csv(extracted, "input/GDD_2016_2017_R.csv")
